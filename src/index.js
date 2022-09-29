c217aec119b7616b65e3d03ea6331352b82ac40b
JAMES ASIBEY MANU <jaamanu@gmail.com>
2022-09-30 00:54:20 +0200

    Add checkbox

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 import './style.css';
 
-alert('Welcome to webpack setup By JAMES');
+const form = document.querySelector('form');
+const todoContainer = document.querySelector('.todoContainer');
+
+const lists = [{
+  description: 'Work hard on coding',
+  bool: 'false',
+  index: '1',
+},
+{
+  description: 'Be smart and consistent',
+  bool: 'false',
+  index: '2',
+}, {
+  description: 'You can do it JAMES ',
+  bool: 'false',
+  index: '3',
+}];
+
+lists.forEach((task) => {
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const ul = document.createElement('ul');
+    const hr = document.createElement('hr');
+    ul.innerHTML = `
+    <li>
+    <input class="checkBox" type="checkbox">${task.description}<i class="fa-solid fa-ellipsis-vertical dots"></i>
+    </li>
+    `;
+    todoContainer.append(ul, hr);
+  });
+});
